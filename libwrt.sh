5cc0b181d762002353ecaf0713eca3f7d4003ff9
wxzy2 <165672074+wxzy2@users.noreply.github.com>
2025-12-02 10:46:38 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -1,3 +1,5 @@
 rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
