9f46235be911dcb2dba61c27b0d3689039f6e4d1
wxzy2 <165672074+wxzy2@users.noreply.github.com>
2026-05-05 17:09:43 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -2,7 +2,7 @@
 #git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 #chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
-
+sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 
 #!/bin/bash
 
